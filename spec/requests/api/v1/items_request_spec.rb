d56b7ca1558ffc09e5e32d811e10fb79202c2695
Richard LaBrecque <RichardlaBreque@gmail.com>
2022-03-29 13:59:45 -0400

    test: setup item creates for spec

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -2,5 +2,19 @@ require 'rails_helper'
 
 RSpec.describe 'Item request Specs' do
   it 'returns all items as json' do
+    merchants = create_list(:merchant, 4)
+    items = merchants.map do |merchant|
+      merchant.items.create([{name: Faker::Lorem.word,
+                      description: Faker::Lorem.sentence,
+                      unit_price:Faker::Number.decimal(l_digits: 2)},
+                      {name: Faker::Lorem.word,
+                      description: Faker::Lorem.sentence,
+                      unit_price:Faker::Number.decimal(l_digits: 2)}]
+    )
+    end
+    binding.pry
+    get '/api/vi/items'
+    expect(response).to be_successful
+    items = JSON.parse(response.body, symbolize_names: true)
   end
 end
