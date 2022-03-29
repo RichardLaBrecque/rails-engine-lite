82e7d2fb1befd8a65cbf57f528c60420f7e9e4d3
Richard LaBrecque <RichardlaBreque@gmail.com>
2022-03-29 17:21:06 -0400

    feat: create items

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -52,8 +52,29 @@ RSpec.describe 'Item request Specs' do
         expect(item[:data][:attributes]).to have_key(:unit_price)
       end
 
-      # it 'creates an item' do
-      #   merchants = create_list(:merchant, 4)
-      #
-      # end
+      it 'creates an item' do
+        merchants = create_list(:merchant, 4)
+        # params = {
+        #   "name": "value1",
+        #   "description": "value2",
+        #   "unit_price": 100.99,
+        #   "merchant_id": merchants.first.id
+        # }
+        post "/api/v1/items", params: {item: {"name": "value1",
+                                              "description": "value2",
+                                              "unit_price": 100.99,
+                                              "merchant_id": merchants.first.id}}
+        expect(response).to be_successful
+        created = JSON.parse(response.body, symbolize_names: true)
+        expect(created).to have_key(:data)
+        expect(created[:data]).to have_key(:id)
+        expect(created[:data]).to have_key(:type)
+        expect(created[:data]).to have_key(:attributes)
+        expect(created[:data][:type]).to eq("item")
+        expect(created[:data][:attributes]).to have_key(:name)
+        expect(created[:data][:attributes]).to have_key(:description)
+        expect(created[:data][:attributes]).to have_key(:unit_price)
+        expect(created[:data][:attributes]).to have_key(:merchant_id)
+
+      end
     end
